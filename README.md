708caf4f7632c8d46e9f9195bef5379c1dcc136f
Matthew M Tarfa <tarfamatthew@gmail.com>
2022-08-12 23:51:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Heads or Tails
+
+A python code that helps with coin toss
+with this code you dont need a coin any more .
---
+++ main.py
@@ -0,0 +1,19 @@
+ #🎲
+import random 
+
+head_or_tail = random.randint(1, 2)
+
+if head_or_tail == 1:
+    print("heads")
+else:
+    print("tails")
+
+
+
+
+
+
+
+
+
+
